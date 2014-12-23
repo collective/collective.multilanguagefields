a1bde22e8ec1c5b28340938196fa38211576dc89
Kees Hink <keeshink@gmail.com>
2014-12-23 11:03:54 +0100

    Use vocabulary SupportedLanguages for dictionary key

--- collective/multilanguagefields/fields.py
+++ collective/multilanguagefields/fields.py
@@ -67,8 +67,9 @@ class TextLine(schema.Dict):
 
     def __init__(self, key_type=None, value_type=None, **kw):
         super(TextLine, self).__init__(**kw)
-        self.key_type = schema.TextLine(
+        self.key_type = schema.Choice(
             title=_(u"Language"),
+            vocabulary="plone.app.vocabularies.SupportedContentLanguages",
         )
         self.value_type = schema.TextLine(
             title=kw.get('title'),
