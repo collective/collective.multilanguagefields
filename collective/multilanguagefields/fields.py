329fe9a37021ba7d8ad1f862857f2a937d2d685c
Kees Hink <keeshink@gmail.com>
2014-12-31 13:19:48 +0100

    Add Text field (multiline, for description etc.)

--- collective/multilanguagefields/fields.py
+++ collective/multilanguagefields/fields.py
@@ -4,16 +4,28 @@ from z3c import form
 from zope.component import adapts
 from zope import schema
 
+language_choice = schema.Choice(
+    title=_(u"Language"),
+    vocabulary="plone.app.vocabularies.SupportedContentLanguages",
+)
+
 
 class MultiLanguageTextLine(schema.Dict):
 
     def __init__(self, key_type=None, value_type=None, **kw):
         super(MultiLanguageTextLine, self).__init__(**kw)
-        self.key_type = schema.Choice(
-            title=_(u"Language"),
-            vocabulary="plone.app.vocabularies.SupportedContentLanguages",
-        )
+        self.key_type = language_choice
         self.value_type = schema.TextLine(
+            title=kw.get('title'),
+        )
+
+
+class MultiLanguageText(schema.Dict):
+
+    def __init__(self, key_type=None, value_type=None, **kw):
+        super(MultiLanguageText, self).__init__(**kw)
+        self.key_type = language_choice
+        self.value_type = schema.Text(
             title=kw.get('title'),
         )
 
